81a9e8a37c4420df0bdb28051f21dffcc02351a5
1831350846 <99323348+1831350846@users.noreply.github.com>
2025-03-10 17:59:49 +0800

    Delete zh directory

--- zh/shijian45.js
+++
@@ -1,8 +0,0 @@
-        function furtherProcess(shi) {
-            // 获取输入框的值（人数）45表
-            let numericValue = parseInt(shi, 10);
-            let acceptance = numericValue - 1;
-                return acceptance;
-
-      
-        }
